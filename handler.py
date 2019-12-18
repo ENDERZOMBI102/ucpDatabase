d7b5cb52a5b524a8e56223d9a7bd5221d20d4223
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2019-12-18 07:38:54 +0000

    modified the response method for creating a package

--- handler.py
+++ handler.py
@@ -1,12 +1,53 @@
 from servepy import *
-class db:
+from json import dumps
+from threading import Thread
+
+class db(Thread):
     def get(req, res):
         with open('./Database.json', 'r') as file:
             tfile = file.read()
             res.status(200).set('Content-Type', 'application/json').send( tfile )
               
     def createPackage(req, res):
-        print(req)
+        # setup the response dict with default values
+        response = {"status": 200,"validUserAgent": True, "validContentType": True, "secure": True, "message": "", "True?": "True"}
+        # check the user agent, if the request isn't done by requests set the validUserAgent flag to false
+        # and the status to 406 (invalid request)
+        if ("python-requests") not in req.headers['USER-AGENT']:
+            response["validUserAgent"] = False
+            response["status"] = 406
+            response["message"] = response["message"] + "\n Invalid user agent."
+        # check the content type, if isn't application/json set the validContentType flag to false
+        # and the status to 406 (invalid request)
+        if req.headers["CONTENT-TYPE"] != "application/json":
+            response["validContentType"] = False
+            response["status"] = 406
+            response["message"] = response["message"] + "\n The content type in the request isn't json."
+        # check if the protocol is https, if it isn't, set the secure flag to false
+        # and the status to 406 (invalid request)
+        if req.headers["X-FORWARDED-PROTO"] != "https":
+            response["secure"] = False
+            response["status"] = 406
+            response["message"] = response["message"] + "\n The protocol used isn't https."
+        # add the package to the database
+        try:
+            reqData = req.content[""]
+        except err:
+            response["status"] = 406
+            response["validContentType"] = False
+            response["message"] = response["message"] + "\n There no json in the request."
+        with open('./Database.json', 'r') as file:
+            data = json.dumps(file)
+            data[]
+
+            
+
+
+
+        # send the response to the client
+        res.status(response["status"]).set('Content-Type', 'application/json').send( response )
+
+        
 
 class server:
     def listening():
--- server.py
+++ server.py
@@ -3,9 +3,12 @@ from handler import *
 
 
 app = App()
-
 router = Router()
-router.get("/Database/get/", db.get)
-router.post("/Database/create/", db.createPackage)
+
+router.get("/API/get/", db.get) # send the client the database
+router.post("/API/create/", db.createPackage) # submit a package to the database
+
+
+
 app.use(router)
 app.listen(port=3000, callback=server.listening)
